80e9bfe7d243feea65c5e576f2ab97abb25e3e0c
EnochHe1 <enoch.he.college@gmail.com>
2024-01-06 00:08:03 -0500

    Nav Bar Draft #1

--- enoch-portfolio-website/src/Components/Nav/Nav.jsx
+++ enoch-portfolio-website/src/Components/Nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { MdLaptopChromebook } from "react-icons/md";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a> {/* # goes to top of the page */}
+      <a href="#about"><AiOutlineUser/></a> {/* href="#some-id" would scroll to an element on the current page such as <div id="some-id"> */}
+      <a href="#experience"><BiBook /></a>
+      <a href="#services"><MdLaptopChromebook /></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
