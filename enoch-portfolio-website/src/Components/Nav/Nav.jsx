d1cdc9c30417d9de8066cf38acca2abd0a2ff808
EnochHe1 <enoch.he.college@gmail.com>
2024-01-06 01:18:01 -0500

    active status navbar updated

--- enoch-portfolio-website/src/Components/Nav/Nav.jsx
+++ enoch-portfolio-website/src/Components/Nav/Nav.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 import "./nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { MdLaptopChromebook } from "react-icons/md";
 import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from 'react';
 
 const Nav = () => {
+  const[activeNav, setActiveNav] = useState('#'); /* need states/local variables to determine the active Nav */
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a> {/* # goes to top of the page */}
-      <a href="#about"><AiOutlineUser/></a> {/* href="#some-id" would scroll to an element on the current page such as <div id="some-id"> */}
-      <a href="#experience"><BiBook /></a>
-      <a href="#services"><MdLaptopChromebook /></a>
-      <a href="#contact"><BiMessageSquareDetail /></a>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiOutlineHome/></a> {/* # goes to top of the page */}
+      <a href="#about" onClick={()=> setActiveNav('#about')} className= {activeNav === "#about" ? 'active' : '' }><AiOutlineUser/></a> {/* href="#some-id" would scroll to an element on the current page such as <div id="some-id"> */}
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className= {activeNav === "#experience" ? 'active' : '' }><BiBook /></a>
+      <a href="#services" onClick={()=> setActiveNav('#services')} className= {activeNav === "#services" ? 'active' : '' }><MdLaptopChromebook /></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className= {activeNav === "#contact" ? 'active' : '' } ><BiMessageSquareDetail /></a>
     </nav>
   )
 }
