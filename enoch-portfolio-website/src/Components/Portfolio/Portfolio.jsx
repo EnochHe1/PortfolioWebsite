16a8eca6927abe47f59fb08465295600c1948cb6
EnochHe1 <enoch.he.college@gmail.com>
2024-01-07 20:30:26 -0500

    temp Portfolio.jsx

--- enoch-portfolio-website/src/Components/Portfolio/Portfolio.jsx
+++ enoch-portfolio-website/src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 import "./portfolio.css";
+import Verbalyst from "../../Assets/Verbalyst.png";
+import AtTune from "../../Assets/AtTune.png";
+import Balancium from "../../Assets/Balancium.png";
+import Nerdle from "../../Assets/Nerdle.png";
+import Pong from "../../Assets/Pong.png";
+import Summazon from "../../Assets/Summazon.png";
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Previous Work</h5>
+      <h2>Projects</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Verbalyst} alt="Verbalyst" />
+          </div>
+          <h3>Verbalyst</h3>
+          <a href="https://github.com/EnochHe1/Verbalyst" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Summazon} alt="Summazon" />
+          </div>
+          <h3>Summazon</h3>
+          <a href="https://github.com/EnochHe1/Summazon" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={AtTune} alt="AtTune" />
+          </div>
+          <h3>AtTune</h3>
+          <a href="https://github.com/EnochHe1/AtTune" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Balancium} alt="Balancium" />
+          </div>
+          <h3>Balancium</h3>
+          <a href="https://github.com/EnochHe1/Balancium" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Pong} alt="Pong" />
+          </div>
+          <h3>Pong</h3>
+          <a href="https://github.com/EnochHe1/Pong" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={Nerdle} alt="Nerdle" />
+          </div>
+          <h3>Nerdle</h3>
+          <a href="https://github.com/EnochHe1/Nerdle" className="btn btn-primary" target="_blank">Github</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
