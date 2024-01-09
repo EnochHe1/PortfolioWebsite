cace0d52b7f7bc4149d27ac95ba2ce1dc31dd247
EnochHe1 <enoch.he.college@gmail.com>
2024-01-09 01:25:53 -0500

    oops

--- enoch-portfolio-website/src/index.js
+++ enoch-portfolio-website/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./src/App.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
