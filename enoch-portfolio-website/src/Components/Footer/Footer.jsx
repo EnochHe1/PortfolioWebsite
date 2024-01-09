655de426e00c14b365b4b36bbc398db3333507da
EnochHe1 <enoch.he.college@gmail.com>
2024-01-09 00:45:11 -0500

    Updated Footer.jsx

--- enoch-portfolio-website/src/Components/Footer/Footer.jsx
+++ enoch-portfolio-website/src/Components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Enoch He</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#achievements">Achievements</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/enoch-he-ab3600198" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a> {/*Target is _blank so it opens in a new tab */}
+        <a href="https://github.com/EnochHe1" target="_blank" rel="noopener noreferrer"><FaGithub /></a> {/*Target is _blank so it opens in a new tab */}
+        <a href="https://www.instagram.com/enoch_he8/" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></a> {/*Target is _blank so it opens in a new tab */}
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Enoch He. All rights reserved.</small>
+      </div>
+
+
+    </footer>
   )
 }
 
