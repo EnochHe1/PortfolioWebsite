ee1526fbbf927c4c119a35f6c4ef1cd15fafc0f0
EnochHe1 <enoch.he.college@gmail.com>
2024-01-08 19:30:47 -0500

    Added swiper.js for pagination

--- enoch-portfolio-website/src/Components/Achievements/Achievements.jsx
+++ enoch-portfolio-website/src/Components/Achievements/Achievements.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import "./achievements.css";
+import {Pagination} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination'; /*Carousel with dots at bottom (navigation doesn't have the dots) */
+import { CiTrophy } from "react-icons/ci";
 
 const data = [
   {
-    achievement: "Best Web Award @ Ignition Hacks (2023)",
-    description: "Out of a Pool of 700+ Hackers at IgnitionHacks v.3, my team and I received the Best Web Award alongside $2750 in prizes total."
+    achievement: "Best Website Award @ Ignition Hacks Hackathon (2023)",
+    description: "Out of a pool of 700+ hackers at IgnitionHacks v.3, my team and I received the Best Web Award alongside $2750 in prizes total for Verbalyst."
   },
   {
     achievement: "UWaterloo Avogadro Contest International Champion (2022)",
-    description: "Out of nearly 5000 students across the world, I placed first with a perfect score on University of Waterloo's chemistry contest.",
+    description: "Out of nearly 5000 students across the world, I placed first with a perfect score on the University of Waterloo's chemistry contest.",
   },
   {
     achievement: "Ninth Place in the CaYPT National Tournament (2022)",
-    description: "As team captain of my school's Canadian Young Physicists' Tournament team, I led our team to place ninth place overall in the national tournament."
+    description: "As team captain of my school's Canadian Young Physicists' Tournament team, I led the team to place ninth nationally in the official tournament."
   }
 ]
 
@@ -22,40 +28,46 @@ const Achievements = () => {
       <h5>What I Have Accomplished</h5>
       <h2>Achievements</h2>
 
-      <div className="container achievements_container">
+      <Swiper className="container achievements_container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
         { /* enter javascript mode  with braces, You can put any valid JavaScript expression inside the curly braces */
           data.map(({achievement, description}, index) => { 
             /*achievement and description are alr variables defined, index is the actual index in the array*/
             return(
-              <article key={index} className="achievement"> 
+              <SwiperSlide key={index} className="achievement"> 
+                <CiTrophy />
                 <h2>{achievement}</h2>
                 <br/>
                 <small>{description}
                 </small>
-            </article>
+            </SwiperSlide>
             )
           })
         }
 
-        <article className="testscores">
+        <SwiperSlide className="testscores">
         <h2>Test Scores </h2>
           <div className= "testscore">
             <br/>
-            SAT: 1520/1600
+            <small>SAT: 1520/1600</small>
             <br/>
-            ACT: 36/36
+            <small>ACT: 36/36</small>
             <br/>
-            AP Calculus BC: 5/5
+            <small>AP Calculus BC: 5/5</small>
             <br/>
-            AP Calculus BC (AB-subscore): 5/5
+            <small>AP Calculus BC (AB-subscore): 5/5</small>
             <br/>
-            AP Computer Science A: 5/5
+            <small>AP Computer Science A: 5/5</small>
             <br/>
-            Euclid Score: 79, 81 (2023, 2022)
+            <small>Euclid Score: 79, 81 (2023, 2022)</small>
           </div>
-        </article>
+        </SwiperSlide>
 
-      </div>
+      </Swiper>
     </section>
   )
 }
