0eb6746398f8bd203cfc5d4fb73d992dba4c5ef5
Enoch He <enoch.he.college@gmail.com>
2024-07-28 19:43:12 -0400

    Made some minor changes

--- enoch-portfolio-website/src/Components/Header/Header.jsx
+++ enoch-portfolio-website/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ import HeaderSocials from './Socials';
 
 const Header = () => {
   return (
-    <header> 
+    <header id="home"> 
       <div className="container header__container">{/*BEM css styling */}
         <h5>Hi, I'm</h5> 
         <h1>Enoch He</h1>
@@ -15,7 +15,7 @@ const Header = () => {
         <HeaderSocials />
 
         <div className="me">
-          <img src={Self} alt="My picture!"/>
+          <img src={Self} alt="Me!"/>
         </div>
 
         <a href="#contacts" className="scroll-down">
--- enoch-portfolio-website/src/Components/Header/Socials.jsx
+++ enoch-portfolio-website/src/Components/Header/Socials.jsx
@@ -6,9 +6,9 @@ import { AiFillInstagram } from "react-icons/ai";
 const Socials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/enoch-he-ab3600198" target="_blank"><BsLinkedin /></a> {/*Target is _blank so it opens in a new tab */}
-        <a href="https://github.com/EnochHe1" target="_blank"><FaGithub /></a> {/*Target is _blank so it opens in a new tab */}
-        <a href="https://www.instagram.com/enoch_he8/" target="_blank"><AiFillInstagram /></a> {/*Target is _blank so it opens in a new tab */}
+        <a href="https://www.linkedin.com/in/enoch-he-ab3600198" target="_blank" rel="noreferrer"><BsLinkedin /></a> {/*Target is _blank so it opens in a new tab */}
+        <a href="https://github.com/EnochHe1" target="_blank" rel="noreferrer"><FaGithub /></a> {/*Target is _blank so it opens in a new tab */}
+        <a href="https://www.instagram.com/enoch_he8/" target="_blank" rel="noreferrer"><AiFillInstagram /></a> {/*Target is _blank so it opens in a new tab */}
     </div>
   )
 }
