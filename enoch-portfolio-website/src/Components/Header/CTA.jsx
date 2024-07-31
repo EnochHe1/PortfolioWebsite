bdfe97e208606eb7fbe25e291c62fc97bac8748b
Enoch He <enoch.he.college@gmail.com>
2024-07-30 23:06:17 -0400

    Button name change

--- enoch-portfolio-website/src/Components/Header/CTA.jsx
+++ enoch-portfolio-website/src/Components/Header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../Assets/Enoch_Resume.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn btn-primary'>CV</a> {}
+        <a href={CV} download className='btn btn-primary'>Download CV</a> {}
         <a href="#contacts" className='btn'>Let's Chat!</a> {/*If you click here, it'll take you to another section */}
     </div>
   )
